345f0d370292431c1517ef92705985a6d2a3235a
iamgraypix <charlesartillero@gmail.com>
2024-06-15 09:50:55 +0800

    remove unused imports

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,10 +6,8 @@ use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Http\Resources\ProductResourceCollection;
-use App\Interfaces\ProductRepositoryInterface;
-use App\Models\Product;
 use App\Services\ProductService;
-use Illuminate\Http\Request;
+
 
 class ProductController extends Controller
 {
